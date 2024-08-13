0f8bbf0a105d99888ddc3025f191fc9e2eda24fa
LAKSHMIPRIYA P K <143695819+lakshmipriyapk@users.noreply.github.com>
2024-08-13 21:18:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript for smooth scrolling or any other interactive features can be added here
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
